858f2b4e1202d299b361c504ea7167f6459ebc4e
Abhishek Kumar Bharti <bhartiabhishek310@gmail.com>
2023-01-25 23:39:42 +0530

    Modified Analyzer software Started to code in jupyter notebook and also put some lines and pause the process the development of the project in the py file instead doing the coding in the jupyter notebook also added refiened_data.csv for the demonstration of refined file

--- analyser_health_management.py
+++ analyser_health_management.py
@@ -3,23 +3,26 @@
 import pandas as pd
 from os.path import expanduser
 import time
+from datetime import datetime,timedelta
+
 
 class Main(object):
 	"""docstring for Main"""
 	def __init__(self):
 		# self.path=f'{expanduser("~")}/app_data.log'
-		self.path='app_data.log'
+		# self.path='app_data.log'
+		self.path='app_data_modified.log'
 		self.log_time_diff=1
 		self.time_format='%H:%M:%S'
 		self.data_dict={'DATE':[],'START TIME':[],'END TIME':[],'USAGE':[],'APPLICATION':[]}
-		# self.data_dict={'DATE':[],'START TIME':[],'END TIME':[],'APPLICATION':[]}
 
 	def time_diff_finder(self,initial_time,final_time):
-		# print(initial_time)
-		# print(final_time)
-		initial_epoch = int(time.mktime(time.strptime(initial_time,self.time_format)))
-		final_epoch = int(time.mktime(time.strptime(final_time,self.time_format)))
-		return (final_epoch-initial_epoch)
+		t1 = datetime.strptime(initial_time, self.time_format)
+		t2 = datetime.strptime(final_time, self.time_format)
+		if t2 < t1:
+		    t2 += timedelta(days=1)
+		delta = t2 - t1
+		return int(delta.total_seconds())
 
 	def end_time_calc(self,time_data):
 		epoch = int(time.mktime(time.strptime(time_data,self.time_format)))
@@ -51,10 +54,31 @@ class Main(object):
 		# print(len(self.data_dict['START TIME']))
 		# print(len(self.data_dict['END TIME']))
 		# print(len(self.data_dict['APPLICATION']))
-		newdf=pd.DataFrame(self.data_dict)
+		self.newdf=pd.DataFrame(self.data_dict)
+		print(self.newdf)
+		# self.newdf.to_csv("refined_data.csv",index=False)
+		# print(newdf.loc[(newdf['APPLICATION']=="Thunar")&(newdf['DATE']=='11-01-2023')])
+		# print(newdf.describe())
+class Analyzer(Main):
+	def __init__(self):
+		self.path="refined_data.csv"
+		
+	def current_day_pie_chart(self):
+		df=pd.read_csv(self.path)
+		# print(df)
+		today = datetime.now().strftime("%d-%m-%Y")
+		# print(today)
+		newdf=df.loc[(df['DATE']==today)]
 		print(newdf)
+
+
+
 
 if __name__ == '__main__':
 	m=Main()
 	# print(m.time_diff_finder('11:49:42','11:50:34'))
-	m.data_cleaner()
+	# print(m.time_diff_finder('23:53:50','00:15:50'))
+	# m.data_cleaner()
+	analyzer=Analyzer()
+	analyzer.current_day_pie_chart()
+
